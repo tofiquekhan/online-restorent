b2c4d5c3d2bdd226d4de9074d335ecb5627d4bf7
Tofique Ahmed  Khan <tofique@webkorps.com>
2022-04-21 12:02:38 +0530

    Initial Commit

---
+++ src/test/java/com/webkorps/onlinerestorent/OnlineRestorentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webkorps.onlinerestorent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRestorentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
